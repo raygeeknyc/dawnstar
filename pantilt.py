022ffbff72c06c35fef598a7909c5c36f100b956
Raymond Blum <rsygeeknyc@gmail.com>
2017-09-20 07:52:37 -0400

    Add demo and debugging code added logging

--- pantilt.py
+++ pantilt.py
@@ -2,6 +2,8 @@
 # sudo apt-get update
 # sudo apt-get install pigpio python-pigpio python3-pigpio
 #
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
 
 # Describe our geometry
 COLS = 4
@@ -13,10 +15,10 @@ PAN_PIN = 20
 TILT_PIN = 21
 
 # Servo dependent values
-X_MIN = 1100
-X_MAX = 1900
-Y_MIN = 1100
-Y_MAX = 1900
+X_MIN = 1000
+X_MAX = 2000
+Y_MIN = 1000
+Y_MAX = 2000
 SERVO_DURATION_STEP = 100
 _COL_SPAN = (X_MAX - X_MIN)
 _ROW_SPAN = (Y_MAX - Y_MIN)
@@ -35,39 +37,39 @@ def tilt(duration):
 
 def servo_write(pin, duration):
     actual_duration = int(duration/SERVO_DURATION_STEP)*SERVO_DURATION_STEP
+    logging.debug("setting pin {} to pulsewidth {}".format(pin, actual_duration))
+    return
     pi.set_servo_pulsewidth(pin, actual_duration)
     
-pi = pigpio.pi()
-pi.set_mode(PAN_PIN, pigpio.OUTPUT)
-pi.set_mode(TILT_PIN, pigpio.OUTPUT)
+def demo():
+    pi = pigpio.pi()
+    pi.set_mode(PAN_PIN, pigpio.OUTPUT)
+    pi.set_mode(TILT_PIN, pigpio.OUTPUT)
 
-print ("Pan mode: ", pi.get_mode(PAN_PIN))
-print("setting to: ",pi.set_servo_pulsewidth(PAN_PIN, 1500))
-print("set to: ",pi.get_servo_pulsewidth(PAN_PIN))
+    print ("Pan mode: ", pi.get_mode(PAN_PIN))
+    print("setting to: ",pi.set_servo_pulsewidth(PAN_PIN, 1500))
+    print("set to: ",pi.get_servo_pulsewidth(PAN_PIN))
 
-time.sleep(1)
-print ("Tilt mode: ", pi.get_mode(TILT_PIN))
-print("setting to: ",pi.set_servo_pulsewidth(TILT_PIN, 1500))
-print("set to: ",pi.get_servo_pulsewidth(TILT_PIN))
+    time.sleep(1)
+    print ("Tilt mode: ", pi.get_mode(TILT_PIN))
+    print("setting to: ",pi.set_servo_pulsewidth(TILT_PIN, 1500))
+    print("set to: ",pi.get_servo_pulsewidth(TILT_PIN))
 
-time.sleep(1)
+    time.sleep(1)
+    print("Tilt setting to: ",pi.set_servo_pulsewidth(TILT_PIN, 1100))
+    print("set to: ",pi.get_servo_pulsewidth(TILT_PIN))
 
-print("Tilt setting to: ",pi.set_servo_pulsewidth(TILT_PIN, 1100))
-print("set to: ",pi.get_servo_pulsewidth(TILT_PIN))
+    time.sleep(1)
+    print("Pan setting to: ",pi.set_servo_pulsewidth(PAN_PIN, 1100))
+    print("set to: ",pi.get_servo_pulsewidth(PAN_PIN))
 
-time.sleep(1)
-print("Pan setting to: ",pi.set_servo_pulsewidth(PAN_PIN, 1100))
-print("set to: ",pi.get_servo_pulsewidth(PAN_PIN))
+    time.sleep(1)
+    print("Tilt setting to: ",pi.set_servo_pulsewidth(TILT_PIN, 1900))
+    print("set to: ",pi.get_servo_pulsewidth(TILT_PIN))
 
-time.sleep(1)
-print("Tilt setting to: ",pi.set_servo_pulsewidth(TILT_PIN, 1900))
-print("set to: ",pi.get_servo_pulsewidth(TILT_PIN))
+    time.sleep(1)
+    print("Pan setting to: ",pi.set_servo_pulsewidth(PAN_PIN, 1900))
+    print("set to: ",pi.get_servo_pulsewidth(PAN_PIN))
 
-time.sleep(1)
-print("Pan setting to: ",pi.set_servo_pulsewidth(PAN_PIN, 1900))
-print("set to: ",pi.get_servo_pulsewidth(PAN_PIN))
-
-
-time.sleep(1)
-
-pi.stop()
+    time.sleep(1)
+    pi.stop()
