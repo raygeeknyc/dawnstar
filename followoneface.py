c8b6483c9df2001dc2c73f93f2a9b083c521166c
Raymond Blum <raygeeknyc@gmail.com>
2017-12-30 22:38:15 -0500

    Capture continuous not working started adding OLED support

--- followoneface.py
+++ followoneface.py
@@ -1,10 +1,12 @@
 import logging
 import time
-logging.getLogger('').setLevel(logging.INFO)
-_DEBUG=False
+logging.getLogger('').setLevel(logging.DEBUG)
+_DEBUG=True
 
 from picamera import PiCamera
 RESOLUTION = (640, 480)
+PIXEL_SHIFT_SENSITIVITY = 30
+TRAINING_SAMPLES = 5
 
 CAPTURE_RATE_FPS = 2
 SLEEPY_DELAY_SECS = 3
@@ -20,29 +22,51 @@ cascade = cv2.CascadeClassifier(CASCADE_PATH)
 import io
 import sys
 
+# Import OLED support
+import Adafruit_SSD1306
+
+RST=24
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+disp.begin()
+disp.clear()
+disp.display()
+
 def calculateImageDifference(prev_image, current_image):
     "Detect changes in the green channel."
     changed_pixels = 0
-    for x in xrange(current_image.w):
-        for y in xrange(current_image.h):
-            if abs(current_image[1][x,y][1] - prev_image[1][x,y][1]) > PIXEL_SHIFT_SENSITIVITY:
+    w, h = current_image.size
+    logging.debug("examining {}x{} image".format(w, h))
+    current_pix = current_image.load()
+    prev_pix = prev_image.load()
+    for x in xrange(w):
+        for y in xrange(h):
+            if abs(current_pix[x,y][1] - prev_pix[x,y][1]) > PIXEL_SHIFT_SENSITIVITY:
                 changed_pixels += 1
     return changed_pixels
 
 def trainMotion(camera):
     logging.debug("Training motion")
-    try:
-        camera.start_preview(fullscreen=False, window=(100,100,camera.resolution[0], camera.resolution[1]))
-        _motion_threshold = 9999
-        getNextFrame()
-        prev_image = captureImage()
-        for i in range(TRAINING_SAMPLES):
-            prev_image = current_image
-            current_image = captureImage()
-            motion = calculateImageDifference(prev_image, current_image)
-            motion_threshold = min(motion, _motion_threshold)
-    finally:
-        _camera.stop_preview()
+    image_buffer = io.BytesIO()
+    _motion_threshold = 9999
+    current_image = None
+    sample = 0
+    for _ in camera.capture_continuous(image_buffer, format='jpeg', use_video_port=True):
+        logging.debug("sample {}".format(sample))
+        prev_image = current_image
+        try:
+            image_buffer.truncate()
+            image_buffer.seek(0)
+        except Exception, e:
+            logging.exception("Error capturing image")
+            time.sleep(CAMERA_ERROR_DELAY_SECS)
+            continue
+        current_image = Image.open(image_buffer)
+        if not prev_image: prev_image = current_image
+        motion = calculateImageDifference(prev_image, current_image)
+        motion_threshold = min(motion, _motion_threshold)
+        sample += 1
+        if sample >= TRAINING_SAMPLES: break
     logging.debug("Trained motion threshold is {}".format(motion_threshold))
     return motion_threshold
 
@@ -51,13 +75,6 @@ def getCamera():
     camera.resolution = RESOLUTION
     camera.vflip = False
     return camera
-
-def captureImage(camera):
-    image_buffer = io.BytesIO()
-    camera.capture(image_buffer, format="jpeg")
-    image_buffer.seek(0)
-    image = Image.open(image_buffer)
-    return image
 
 # Take a PIL RGB image, return a list of faces
 def findFaces(image):
@@ -91,32 +108,43 @@ def showImage(image):
 if __name__ == '__main__':
     logging.info("finding a face")
     camera = getCamera()
+    motion_threshold = trainMotion(camera)
     frame_delay_secs = 1.0/CAPTURE_RATE_FPS
-    rgb_image = captureImage(camera)
     last_motion_at = 0l
     last_frame_at = 0l
     just_moved = False
-    while True:
-        delay = (last_frame_at + frame_delay_secs) - time.time()
-        if delay > 0:
-            time.sleep(delay)
-        prev_image = rgb_image
-        rgb_image = captureImage(camera)
+    display_image = None
+    motion = motion_threshold
+    image_buffer = io.BytesIO()
+
+    for _ in camera.capture_continuous(image_buffer, format='jpeg', use_video_port=True):
+        prev_image = display_image
+        try:
+            image_buffer.truncate()
+            image_buffer.seek(0)
+        except Exception, e:
+            logging.exception("Error capturing image")
+            time.sleep(CAMERA_ERROR_DELAY_SECS)
+            continue
+        display_image = Image.open(image_buffer)
         last_frame_at = time.time()
-        if _DEBUG: showImage(rgb_image)
         if just_moved:
             just_moved = False
             continue
-        motion = calculateImageDifference(prev_image, rgb_image)
+        if prev_image:
+            motion = calculateImageDifference(prev_image, display_image)
         if motion < motion_threshold:
             if time.time() < (last_motion_at + SLEEPY_DELAY_SECS):
                 pass  # indicate that we're bored or sleeping
             continue
+        logging.debug("motion")
         last_motion_at = time.time()
-        faces = findFaces(rgb_image)
-        face = findOneFace(faces)
+        faces = findFaces(display_image)
+        face = None
+        if len(faces) > 0:
+            face = findOneFace(faces)
         if not face:
             logging.debug("no face seen")
             continue
         face_center = (face[0]+(face[2]/2), face[1]+(face[3]/2))
-        logging.info("face center is {},{}".format(face_center))
+        logging.info("face center is {},{}".format(face_center[0], face_center[1]))
