29a3454db97dab93f63ac020d40aca9b9b9d954b
Raymond Blum <raygeeknyc@gmail.com>
2018-02-27 23:16:45 -0500

    Added eval of brightness and histogram equalized frames

--- trackface.py
+++ trackface.py
@@ -1,5 +1,5 @@
 import logging
-_DEBUG=True
+_DEBUG=False
 if _DEBUG:
   logging.getLogger().setLevel(logging.DEBUG)
 else:
@@ -12,6 +12,18 @@ from followface import findFaces, findOneFace, getCenteringCorrection, frameFace
 from pantilt import pointTo
 pan_tilt_state = [None, None]
 RESOLUTION=(640, 480)
+
+def equalize_brightness(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv[:,:,2] = cv2.equalizeHist(hsv[:,:,2])
+    eq_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return eq_img
+
+def equalize_hist(img):
+    eq_img = img.copy()
+    for c in xrange(0, 2):
+       eq_img[:,:,c] = cv2.equalizeHist(img[:,:,c])
+    return eq_img
 
 faces = 0
 frames = 0
@@ -27,8 +39,12 @@ while(True):
     # Capture frame-by-frame
     ret, frame = videostream.read()
     frames += 1
+    eq_brightness_frame = equalize_brightness(frame)
+    eq_contrast_frame = equalize_hist(frame)
     faces = findFaces(frame, classifier, profile_classifier)
-    logging.info("{} faces".format(len(faces)))
+    eq_contrast_faces = findFaces(eq_contrast_frame, classifier, profile_classifier)
+    eq_brightness_faces = findFaces(eq_brightness_frame, classifier, profile_classifier)
+    logging.info("{} faces {} eq_contrast {} eq_brightness".format(len(faces), len(eq_contrast_faces), len(eq_brightness_faces)))
     if len(faces) > 0:
         face = findOneFace(faces)
         (face_center, look_dir) = getCenteringCorrection(face, RESOLUTION)
