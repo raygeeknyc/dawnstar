79cc40e2c461515c974017a3a62c21b7152104cf
Raymond Blum <raygeeknyc@gmail.com>
2018-04-30 08:04:54 -0400

    Cleaned up the multiprocessing model code for the Detector class Added some of the model initilization from my demo code Not compiled

--- object_detection/detector.py
+++ object_detection/detector.py
@@ -10,32 +10,9 @@ import time
 import io
 import sys
 import os, signal
-Last login: Fri Mar 30 07:07:15 on ttys000
-Raymonds-MacBook:~ raymond$ cd Documents/workspace/dawnstar/object_detection/
-Raymonds-MacBook:object_detection raymond$ cp classify_video.py x.py
-Raymonds-MacBook:object_detection raymond$ vi x.py 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 import numpy as np
-import os
 import six.moves.urllib as urllib
 import sys
 import tarfile
@@ -66,9 +43,9 @@ class Detector(multiprocessing.Process):
         self._log_queue = log_queue
         self._log_level = log_level
         self._transcript = i
-        self._stop_producing = False
         self._stop_processing = False
         self._work_queue = deque()
+        self._PrepareModel()
 
     def _initLogging(self):
         handler = multiprocessingloghandler.ChildMultiProcessingLogHandler(self._log_queue)
@@ -79,19 +56,64 @@ class Detector(multiprocessing.Process):
         logging.debug("***background received shutdown")
         self._exit.set()
 
+    def _PrepareModel(self):
+        # By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+
+        # What model to download.
+        MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+        MODEL_FILE = MODEL_NAME + '.tar.gz'
+        DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+        # Path to frozen detection graph. This is the actual model that is used for the object detection.
+        PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+        
+        # List of the strings that is used to add correct label for each box.
+        PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+        
+        NUM_CLASSES = 90
+        
+        # ## Download Model if it's not present 
+        if not os.path.exists(MODEL_NAME):
+          logging.info("Downloading {}".format(MODEL_FILE))
+          opener = urllib.request.URLopener()
+          opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+          logging.info("Unpacking {}".format(MODEL_NAME))
+          tar_file = tarfile.open(MODEL_FILE)
+          for file in tar_file.getmembers():
+            file_name = os.path.basename(file.name)
+            if 'frozen_inference_graph.pb' in file_name:
+              tar_file.extract(file, os.getcwd())
+          try:
+            logging.info("Removing {}".format(MODEL_FILE))
+            os.remove(MODEL_FILE)
+          except OSError:
+            logging.error("Error removing {}".format(MODEL_FILE))
+        
+        # ## Load a (frozen) Tensorflow model into memory.
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+          od_graph_def = tf.GraphDef()
+          with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        
+        
+        # ## Loading label map
+        # Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+        
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+        category_index = label_map_util.create_category_index(categories)
+        
     def run(self):
         try:
             logging.debug("***background active")
             logging.debug("process %s (%d)" % (self.name, os.getpid()))
-            logging.debug("creating producer")
-            self._producer = threading.Thread(target=self.produceWork)
             logging.debug("creating processor")
             self._processor = threading.Thread(target=self.performWork)
             self._initLogging()
             logging.debug("starting processor")
             self._processor.start()
-            logging.debug("starting producer")
-            self._producer.start()
             logging.debug("waiting for exit event")
             self._exit.wait()
             logging.debug("exit event received")
@@ -99,25 +121,11 @@ class Detector(multiprocessing.Process):
         except Exception, e:
             logging.error("***background exception: {}".format(e))
         logging.debug("***background terminating")
-        self._stopProducing()
         self._stopProcessing()
-        self._producer.join()
         self._processor.join()
 
-    def _stopProducing(self):
-        self._stop_producing = True
-    
     def _stopProcessing(self):
         self._stop_processing = True
-
-    def produceWork(self):
-        logging.debug("producing")
-        i = 0
-        while not self._stop_producing:
-            self._work_queue.append(i)
-            time.sleep(1)
-            i+=1
-        logging.debug("stopped producing")
 
     def performWork(self):
         logging.debug("performing")
@@ -138,7 +146,7 @@ if __name__ == '__main__':
 
     logging.debug("starting main")
     transcript = multiprocessing.Pipe()
-    background_process = Background(transcript, log_queue, logging.getLogger('').getEffectiveLevel())
+    background_process = Detector(transcript, log_queue, logging.getLogger('').getEffectiveLevel())
     try:
         i, o = transcript
         background_process.start()
