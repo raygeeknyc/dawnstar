79d872ef86120c3387f9f135b7790ff0787bfd4d
raygeeknyc <raygeeknyc@gmail.com>
2018-10-12 08:19:11 -0400

    Encapsulate display add main module untried

---
+++ dawnstar.py
@@ -0,0 +1,28 @@
+import logging
+logging.getLogger('').setLevel(logging.DEBUG)
+
+import os
+import subprocess
+
+from display import DisplayInfo, Display
+
+class Dawnstar(object):
+  _IP_CMD = 'hostname -I | cut -d\" \" -f1'
+
+  def __init__(self):
+    self._ip_address = None
+
+  def get_ip_address(self):
+    if not self._ip_address:
+      self._ip_address = subprocess.check_output(Dawnstar._IP_CMD, shell = True )
+      logging.debug('IP {}'.format(self._ip_address))
+      return self._ip_address
+
+robot = Dawnstar()
+print('Ip address: {}'.format(robot.get_ip_address()))
+
+screen = Display()
+
+info = DisplayInfo()
+info.ip = robot.get_ip_address
+screen.show(info)
--- display.py
+++ display.py
@@ -10,7 +10,6 @@ import logging
 logging.getLogger('').setLevel(logging.DEBUG)
 
 import time
-import subprocess
 
 import Adafruit_SSD1306
 
@@ -19,16 +18,6 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 _RST_PIN = 24
-
-def setup_display():
-  # 128x64 display with hardware I2C:
-  disp = Adafruit_SSD1306.SSD1306_128_64(rst=_RST_PIN)
-  disp.begin()
-  # Clear display.
-  disp.clear()
-  disp.display()
-  logging.debug('/setup_display')
-  return disp
 
 class DisplayInfo(object):
   def __init__():
@@ -39,65 +28,70 @@ class DisplayInfo(object):
     self.tracking_bounds = (0, 0)
     self.tracking_zone = (0, 0)
 
-def update_display(display, display_info):
-  # Create blank image for drawing.
-  # Make sure to create image with mode '1' for 1-bit color.
-  width = disp.width
-  height = disp.height
-  image = Image.new('1', (width, height))
-  logging.debug('display {} x {}'.format(width, height))
+class Display(object):
+  def _setup_display():
+    # 128x64 display with hardware I2C:
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=_RST_PIN)
+    disp.begin()
+    # Clear display.
+    disp.clear()
+    disp.display()
+    logging.debug('/setup_display')
+    return disp
 
-  # Get drawing object to draw on image.
-  draw = ImageDraw.Draw(image)
+  def __init__(self, info):
+    self._info = info
+    self._screen = Display._setup_display()
 
-  # Draw a black filled box to clear the image.
-  draw.rectangle((0,0,width,height), outline=0, fill=0)
+  def show(display_info):
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    width = self._screen.width
+    height = self._screen.height
+    image = Image.new('1', (width, height))
+    logging.debug('display {} x {}'.format(width, height))
 
-  x = 0
-  y = 0
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-  ## Load default font.
-  # font = ImageFont.load_default()
-  font = graphics.Font()
-  font.LoadFont('fonts/5x7.bdf')
-  line_height = font.getsize()[1]
-  logging.debug('Font: {} x {}'.format(font.getSize()[0], font.getSize()[1]))
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-  draw.rectangle((0,0,width,height), outline=0, fill=0)
+    x = 0
+    y = 0
 
-  draw.text((x, y), 'IP: ' + display_info.ip,  font=font, fill=255)
-  logging.debug('IP: {}'.format(str(display_info.ip)))
+    ## Load default font.
+    # font = ImageFont.load_default()
+    font = graphics.Font()
+    font.LoadFont('fonts/5x7.bdf')
+    line_height = font.getsize()[1]
+    logging.debug('Font: {} x {}'.format(font.getSize()[0], font.getSize()[1]))
 
-  faces = 0
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-  y += font_height + 1
-  draw.text((x, y), 'Faces: {}'.format(display_info.faces), font=font, fill=255)
-  logging.debug('Faces: {}'.format(display_info.faces))
+    draw.text((x, y), 'IP: ' + display_info.ip,  font=font, fill=255)
+    logging.debug('IP: {}'.format(str(display_info.ip)))
 
-  face_bounds = (0,0)
-  face_zone = (0,0)
+    faces = 0
 
-  y += font_height + 1
-  draw.text((x, y), 'Tracking: {},{}  Zone: {}'.format(display_info.tracking_bounds, display_info.tracking_zone), font=font, fill=255)
-  logging.debug('Faces: {}'.format(display_info.tracking_bounds, display_info.tracking_zone))
+    y += font_height + 1
+    draw.text((x, y), 'Faces: {}'.format(display_info.faces), font=font, fill=255)
+    logging.debug('Faces: {}'.format(display_info.faces))
 
-  left_motor_state = 0
-  right_motor_state = 0
+    face_bounds = (0,0)
+    face_zone = (0,0)
 
-  y += font_height + 1
-  y += font_height + 1
-  draw.text((x, y), 'Left motor: {}, Right motor: {}'.format(display_info.left_motor, display_info.right_motor), font=font, fill=255)
-  logging.debug('Left motor: {}, Right motor: {}: {}'.format(display_info.left_motor, display_info.right_motor))
+    y += font_height + 1
+    draw.text((x, y), 'Tracking: {},{}  Zone: {}'.format(display_info.tracking_bounds, display_info.tracking_zone), font=font, fill=255)
+    logging.debug('Faces: {}'.format(display_info.tracking_bounds, display_info.tracking_zone))
 
-  disp.image(image)
-  disp.display()
+    left_motor_state = 0
+    right_motor_state = 0
 
-def main():
-  blank_info = DisplayInfo()
-  display = setup_display()
-  update_display(display, display_info)
+    y += font_height + 1
+    y += font_height + 1
+    draw.text((x, y), 'Left motor: {}, Right motor: {}'.format(display_info.left_motor, display_info.right_motor), font=font, fill=255)
+    logging.debug('Left motor: {}, Right motor: {}: {}'.format(display_info.left_motor, display_info.right_motor))
 
-
-if __name__ == "__main__":
-  logging.info('display running as main')
-  main()
+    self._screen.image(image)
+    self._screen.display()
