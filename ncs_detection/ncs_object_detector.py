cc5056cbab2a084844c51e901f7bc6e8b56e14b2
Raymond Blum <raygeeknyc@gmail.com>
2018-11-10 08:12:31 -0500

    Add in dropped(?) files Removed unneeded imports

---
+++ ncs_detection/ncs_object_detector.py
@@ -0,0 +1,125 @@
+from mvnc import mvncapi as mvnc
+import numpy as np
+import time
+import cv2
+
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+class NCSObjectClassifier(object):
+	# frame dimensions should be square
+	PREPROCESS_DIMS = (300, 300)
+
+        device = None
+
+        @classmethod
+	def _init_NCS_device(cls):
+		# grab a list of all NCS devices plugged in to USB
+		print("finding NCS devices...")
+		devices = mvnc.EnumerateDevices()
+
+		# if no devices found, exit the script
+		if len(devices) == 0:
+			raise Exception("No devices found. Please plug in a NCS")
+
+		print("found {} devices. device0 will be used. "
+			"opening device0...".format(len(devices)))
+		device = mvnc.Device(devices[0])
+		device.OpenDevice()
+		print("device opened")
+		cls.device = device
+
+	# initialize the list of class labels our network was trained to
+	# detect, then generate a set of bounding box colors for each class
+	CLASSES = ("background", "aeroplane", "bicycle", "bird",
+		"boat", "bottle", "bus", "car", "cat", "chair", "cow",
+		"diningtable", "dog", "horse", "motorbike", "person",
+		"pottedplant", "sheep", "sofa", "train", "tvmonitor")
+
+        
+	def __init__(self, graph_filename, min_confidence):
+		self.graph_filename = graph_filename
+		self.confidence_threshold = min_confidence
+                if not self.__class__.device:
+			self.__class__._init_NCS_device()
+		self._init_graph()
+
+	def cleanup(self):
+		# clean up the graph and device
+		self._graph.DeallocateGraph()
+		self.__class__.device.CloseDevice()
+		self.__class__.device = None
+
+	def _init_graph(self):
+		# open the CNN graph file
+		logging.info("loading the graph file into RPi memory...")
+		with open(self.graph_filename, mode="rb") as f:
+			graph_in_memory = f.read()
+
+		# load the graph into the NCS
+		logging.info("allocating the graph on the NCS...")
+		self._graph = self.__class__.device.AllocateGraph(graph_in_memory)
+
+	@staticmethod
+	def preprocess_image(input_image):
+		# preprocess the image
+		preprocessed = cv2.resize(input_image, NCSObjectClassifier.PREPROCESS_DIMS)
+		preprocessed = preprocessed - 127.5
+		preprocessed = preprocessed * 0.007843
+		preprocessed = preprocessed.astype(np.float16)
+
+		# return the image to the calling function
+		return preprocessed
+
+	def predict(self, image):
+		# preprocess the image
+		image = self.preprocess_image(image)
+
+		# send the image to the NCS and run a forward pass to grab the
+		# network predictions
+		self._graph.LoadTensor(image, None)
+		(output, _) = self._graph.GetResult()
+
+		# grab the number of valid object predictions from the output,
+		# then initialize the list of predictions
+		num_valid_boxes = output[0]
+		predictions = []
+	
+		# loop over results
+		for box_index in range(num_valid_boxes):
+			# calculate the base index into our array so we can extract
+			# bounding box information
+			base_index = 7 + box_index * 7
+
+			# boxes with non-finite (inf, nan, etc) numbers must be ignored
+			if (not np.isfinite(output[base_index]) or
+				not np.isfinite(output[base_index + 1]) or
+				not np.isfinite(output[base_index + 2]) or
+				not np.isfinite(output[base_index + 3]) or
+				not np.isfinite(output[base_index + 4]) or
+				not np.isfinite(output[base_index + 5]) or
+				not np.isfinite(output[base_index + 6])):
+				continue
+
+			# extract the image width and height and clip the boxes to the
+			# image size in case network returns boxes outside of the image
+			# boundaries
+			(h, w) = image.shape[:2]
+			x1 = max(0, int(output[base_index + 3] * w))
+			y1 = max(0, int(output[base_index + 4] * h))
+			x2 = min(w,	int(output[base_index + 5] * w))
+			y2 = min(h,	int(output[base_index + 6] * h))
+
+			# grab the prediction class label, confidence (i.e., probability),
+			# and bounding box (x, y)-coordinates
+			pred_class = int(output[base_index + 1])
+			pred_conf = output[base_index + 2]
+			pred_boxpts = ((x1, y1), (x2, y2))
+
+			# create prediciton tuple and append the prediction to the
+			# predictions list
+			prediction = (pred_class, pred_conf, pred_boxpts)
+			predictions.append(prediction)
+
+		# return the list of predictions to the calling function
+		return predictions
---
+++ ncs_detection/realtime_detection.py
@@ -0,0 +1,124 @@
+# USAGE
+# python ncs_realtime_objectdetection.py --graph graphs/mobilenetgraph --display 1
+# python ncs_realtime_objectdetection.py --graph graphs/mobilenetgraph --confidence 0.5 --display 1
+
+import logging
+logging.getLogger().setLevel(logging.DEBUG)
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import argparse
+import time
+import cv2
+from ncs_object_detector import NCSObjectClassifier
+
+# frame dimensions should be square
+DISPLAY_DIMS = (900, 900)
+PREPROCESS_DIMS = (300, 300)
+
+# calculate the multiplier needed to scale the bounding boxes
+DISP_MULTIPLIER = DISPLAY_DIMS[0] // PREPROCESS_DIMS[0]
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-g", "--graph", required=True,
+	help="path to input graph file")
+ap.add_argument("-c", "--confidence", default=.5,
+	help="confidence threshold")
+ap.add_argument("-d", "--display", type=int, default=0,
+	help="switch to display image on screen")
+args = vars(ap.parse_args())
+
+# open a pointer to the video stream thread and allow the buffer to
+# start to fill, then start the FPS counter
+logging.info("starting the video stream and FPS counter...")
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(1)
+fps = FPS().start()
+
+# loop over frames from the video file stream
+classifier = NCSObjectClassifier(args["graph"], args["confidence"])
+while True:
+	try:
+		# grab the frame from the threaded video stream
+		# make a copy of the frame and resize it for display/video purposes
+		frame = vs.read()
+		image_for_result = frame.copy()
+		image_for_result = cv2.resize(image_for_result, DISPLAY_DIMS)
+
+		# use the NCS to acquire predictions
+		predictions = classifier.predict(frame)
+
+		# loop over our predictions
+		for (i, pred) in enumerate(predictions):
+			# extract prediction data for readability
+			(pred_class, pred_conf, pred_boxpts) = pred
+
+			# print prediction to terminal
+			logging.info("Prediction #{}: class={}, confidence={}, "
+				"boxpoints={}".format(i, NCSObjectClassifier.CLASSES[pred_class], pred_conf,
+					pred_boxpts))
+
+			# check if we should show the prediction data
+			# on the frame
+			if args["display"] > 0:
+				# build a label consisting of the predicted class and
+				# associated probability
+				label = "{}: {:.2f}%".format(NCSObjectClassifier.CLASSES[pred_class],
+					pred_conf * 100)
+
+				# extract information from the prediction boxpoints
+				(ptA, ptB) = (pred_boxpts[0], pred_boxpts[1])
+				ptA = (ptA[0] * DISP_MULTIPLIER, ptA[1] * DISP_MULTIPLIER)
+				ptB = (ptB[0] * DISP_MULTIPLIER, ptB[1] * DISP_MULTIPLIER)
+				(startX, startY) = (ptA[0], ptA[1])
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+
+				# display the rectangle and label text
+				cv2.rectangle(image_for_result, ptA, ptB,
+					COLORS[pred_class], 2)
+				cv2.putText(image_for_result, label, (startX, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 1, COLORS[pred_class], 3)
+
+		# check if we should display the frame on the screen
+		# with prediction data (you can achieve faster FPS if you
+		# do not output to the screen)
+		if args["display"] > 0:
+			# display the frame to the screen
+			cv2.imshow("Output", image_for_result)
+			key = cv2.waitKey(1) & 0xFF
+
+			# if the `q` key was pressed, break from the loop
+			if key == ord("q"):
+				logging.info("break")
+				break
+
+		# update the FPS counter
+		fps.update()
+	
+	# if "ctrl+c" is pressed in the terminal, break from the loop
+	except KeyboardInterrupt:
+		logging.info("break")
+		break
+
+	# if there's a problem reading a frame, break gracefully
+	except AttributeError, e:
+		logging.exception("problem reading frame")
+		break
+
+# stop the FPS counter timer
+fps.stop()
+
+# destroy all windows if we are displaying them
+if args["display"] > 0:
+	cv2.destroyAllWindows()
+
+# stop the video stream
+vs.stop()
+
+classifier.cleanup()
+
+# display FPS information
+logging.info("elapsed time: {:.2f}".format(fps.elapsed()))
+logging.info("approx. FPS: {:.2f}".format(fps.fps()))
