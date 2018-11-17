7a16147591281f60086f64e6aec184115d16f61e
Raymond Blum <raygeeknyc@gmail.com>
2018-11-17 11:45:32 -0500

    More refactoring and fixing Now gets and displays data from the slave device No errors on sending

--- ambulator.py
+++ ambulator.py
@@ -15,10 +15,13 @@ import smbus
 I2C_BUS = 1
 SLAVE_DEVICE_ADDRESS = 0x15 #7 bit address (will be left shifted to add the read write bit)
 
+_CMD_RIGHT = ord('R')
+_CMD_LEFT = ord('L')
+
 class Ambulator():
-    def __init__(self, slave_device):
+    def __init__(self, slave_device_address):
         self._bus = smbus.SMBus(I2C_BUS)
-        self._slave_device = slave_device
+        self._slave_device_address = slave_device_address
 
     def stop(self):
         self.right(REST_SPEED)
@@ -35,24 +38,26 @@ class Ambulator():
     def right(self, speed):
         speed = min(speed, MAX_SPEED)
         speed = max(speed, MIN_SPEED)
-        self._send_command(CMD_RIGHT, speed)
+        self._send_command(_CMD_RIGHT, speed)
 
     def left(self, speed):
         speed = min(speed, MAX_SPEED)
         speed = max(speed, MIN_SPEED)
-        self._send_command(CMD_LEFT, speed)
+        self._send_command(_CMD_LEFT, speed)
 
     def _send_command(self, command, value):
-        self._bus.write_i2c_block_data(self._slave_device, command, value)
+        self._bus.write_i2c_block_data(self._slave_device_address, command, [value])
 
     def get_values(self):
         command = 0
-        value = self._bus.read_i2c_block_data(self._slave_device, command)
+        value = self._bus.read_i2c_block_data(self._slave_device_address, command)
         logging.info("get_value returned {}".format(value))
         return value
 
 def main():
   walker = Ambulator(SLAVE_DEVICE_ADDRESS)
+  walker.forward(500)
+  walker.get_values()
   sys.exit(0)
 
 logging.getLogger().setLevel(_DEBUG)
