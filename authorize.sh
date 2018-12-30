61a9d6d4eb2818b10a4205f669dc55c2fab8f59e
Raymond Blum <raygeeknyc@gmail.com>
2018-12-30 17:23:16 -0500

    Add script to set credentials on this client

---
+++ authorize.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+google-oauthlib-tool --scope https://www.googleapis.com/auth/assistant-sdk-prototype       --scope https://www.googleapis.com/auth/gcm       --save --headless --client-secrets ~/client_secret_85657733836-9gq6h023f0ikj38jmp0mq48ot59smun7.apps.googleusercontent.com.json
